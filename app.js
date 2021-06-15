a2f43c49d87b2c51ca4e5d0eb67af231b1f01e02
zopaj63 <zoran.pajnic@gmail.com>
2021-06-15 20:40:29 +0200

    y string

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ lastName = "Pajnic";
 fullName = firstName + " " + lastName;
 console.log(fullName);
 
-/*
 var x = 2;
 var z = x;
 if (z === 2) {
-    console.log(y);
+    console.log("y");
+}
 else {
     console.log(5);
-    }
 }
-*/
 
 for (i = 1; i <= 20; i++) {
     if (i % 2 === 0) {
