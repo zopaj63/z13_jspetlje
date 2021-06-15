c4251b63c6670b76facb1c4bf51de6a322d94120
zopaj63 <zoran.pajnic@gmail.com>
2021-06-15 20:24:16 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+firstName = "Zoran";
+lastName = "Pajnic";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+
+/*
+var x = 2;
+var z = 2;
+if (x === 2) {
+    console.log(y);
+    else {
+        console.log(5);
+    }
+}
+*/
+
+for (i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log("Broj " + i + " je paran");
+    }
+    else {
+        console.log("Broj " + i + " je neparan");
+    }
+}
